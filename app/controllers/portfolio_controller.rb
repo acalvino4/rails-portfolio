30ca6c32d392f28d2770b53b71296719270620e1
Augustine Calvino <acalvino4@Augustines-MBP.hsd1.co.comcast.net>
2020-07-21 12:39:37 -0600

    add project tiles to portfolio page

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -4,6 +4,7 @@ class PortfolioController < ApplicationController
         @images = Dir.chdir(Rails.root.join('app/assets/images')) do
             Dir.glob("screenshots/*.png")
         end
+        @projects = Project.all
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(title: 'Broadway', image: 'screenshots/broadway.png', description: "Contract us for your next site!")
+Project.create(title: 'Bolt', image: 'screenshots/bolt.png', description: 'The new streaming service in town')
+Project.create(title: 'Innovation Cloud', image: 'screenshots/innovation.png', description: 'The top conference on evolving tech')
+Project.create(title: 'Threadly', image: 'screenshots/threadly.png', description: 'A place for everyone to share their thoughts')
+Project.create(title: 'Bookmarks', image: 'screenshots/bookmarks.png', description: 'See what books are loved, hated, and ignored')
+Project.create(title: 'Bass Music', image: 'screenshots/bass.png', description: 'Browse the best hits in modern music')
